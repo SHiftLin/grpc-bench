aa9c94d63fea960f38a16137bed63db4ea9b94bc
lsh <lshzy137@gmail.com>
2022-02-07 21:42:59 -0500

    add release flag

--- make.sh
+++ make.sh
@@ -1,3 +1,6 @@
 cd build
-cmake -DCMAKE_PREFIX_PATH=$HOME/.local ../
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$HOME/.local ../
 make
+
+#cmake -DCMAKE_PREFIX_PATH=$HOME/.local -DCMAKE_BUILD_TYPE=Release -Bbuild
+#cmake --build build
